ea4fa786b9a0083fe6ace954476eacf0234c8a9f
badmitry <dmitrij88@bk.ru>
2020-07-24 13:26:41 +0300

    1

---
+++ app/src/main/java/badmitry/hellogeekbrains/MainActivity.java
@@ -0,0 +1,38 @@
+package badmitry.hellogeekbrains;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+Random random = new Random();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showWeather(View view) {
+        int i = random.nextInt(2);
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        System.out.println(i);
+        if (i == 1) {
+           textView.setText(R.string.sun);
+           imageView.setImageResource(R.drawable.sun);
+        } else {
+            textView.setText(R.string.rainy);
+            imageView.setImageResource(R.drawable.rain);
+        }
+    }
+//    ImageView imageView = findViewById(R.id.image);
+//    ActionBar.LayoutParams param = (ActionBar.LayoutParams) imageView.getLayoutParams();
+//    param.width = 120;
+//    imageView.
+}
