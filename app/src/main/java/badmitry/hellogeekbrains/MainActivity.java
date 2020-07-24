77f0c4a7b7934f07d4652da2d5b9da4580732ac6
badmitry <dmitrij88@bk.ru>
2020-07-24 15:04:56 +0300

    3

--- app/src/main/java/badmitry/hellogeekbrains/MainActivity.java
+++ app/src/main/java/badmitry/hellogeekbrains/MainActivity.java
@@ -10,7 +10,8 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-Random random = new Random();
+    Random random = new Random();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +20,19 @@ Random random = new Random();
 
     public void showWeather(View view) {
         int i = random.nextInt(2);
+        int t = random.nextInt(15);
         TextView textView = (TextView) findViewById(R.id.textView2);
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
         ImageView imageView = (ImageView) findViewById(R.id.image);
         System.out.println(i);
+        String weather;
         if (i == 1) {
-           textView.setText(R.string.sun);
-           imageView.setImageResource(R.drawable.sun);
+            textView.setText(R.string.sun);
+            textView3.setText((t + 15) + "*C");
+            imageView.setImageResource(R.drawable.sun);
         } else {
             textView.setText(R.string.rainy);
+            textView3.setText((t + 15) + "*C");
             imageView.setImageResource(R.drawable.rain);
         }
     }
