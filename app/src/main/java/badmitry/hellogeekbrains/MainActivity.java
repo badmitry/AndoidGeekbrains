1112b3f350a91bd4b2c03ce4b6caffc00023af49
badmitry <dmitrij88@bk.ru>
2020-07-27 15:12:41 +0300

    5

--- app/src/main/java/badmitry/hellogeekbrains/MainActivity.java
+++ app/src/main/java/badmitry/hellogeekbrains/MainActivity.java
@@ -53,24 +53,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-//    public void showWeather(View view) {
-//        int i = random.nextInt(2);
-//        int t = random.nextInt(15);
-//        TextView textView = (TextView) findViewById(R.id.textView2);
-//        TextView textView3 = (TextView) findViewById(R.id.textView3);
-//        ImageView imageView = (ImageView) findViewById(R.id.image);
-//        System.out.println(i);
-//        String weather;
-//        if (i == 1) {
-//            textView.setText(R.string.sun);
-//            textView3.setText((t + 15) + "*C");
-//            imageView.setImageResource(R.drawable.sun);
-//        } else {
-//            textView.setText(R.string.rainy);
-//            textView3.setText((t + 15) + "*C");
-//            imageView.setImageResource(R.drawable.rain);
-//        }
-//    }
 }
