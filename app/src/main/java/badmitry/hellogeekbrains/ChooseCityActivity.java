c0367e93486c1b9e218b177103b5d299444f6c57
badmitry <dmitrij88@bk.ru>
2020-08-10 19:49:13 +0300

    hw5

--- app/src/main/java/badmitry/hellogeekbrains/ChooseCityActivity.java
+++ app/src/main/java/badmitry/hellogeekbrains/ChooseCityActivity.java
@@ -3,6 +3,7 @@ package badmitry.hellogeekbrains;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MenuItem;
@@ -23,6 +24,10 @@ public class ChooseCityActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+            return;
+        }
         setContentView(R.layout.city);
         initViews();
         isDarkTheme = getIntent().getBooleanExtra("isDarkTheme", false);
